a62c1ed4f9d5b88c5521a5c2be8f791001040e15
Atnhai <atnhai@gmail.com>
2023-04-16 06:33:06 +0700

    add take picture button

---
+++ src/screens/SaveQRcomponent/SaveQRScreen.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {
+  Text,
+  View,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Button,
+  Pressable,
+  TouchableOpacity,
+  Alert,
+  TextInput,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
--- src/screens/ScanQRcomponent/ScanQRScreen.js
+++ src/screens/ScanQRcomponent/ScanQRScreen.js
@@ -1,26 +1,18 @@
 import * as React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useCameraDevices } from 'react-native-vision-camera';
+import { Camera } from 'react-native-vision-camera';
+import { useScanBarcodes, BarcodeFormat } from 'vision-camera-code-scanner';
 
-import {StyleSheet, Text} from 'react-native';
-import {useCameraDevices} from 'react-native-vision-camera';
-import {Camera} from 'react-native-vision-camera';
-import {useScanBarcodes, BarcodeFormat} from 'vision-camera-code-scanner';
-
-export default function ScanQRScreen({navigation}) {
+export default function ScanQRScreen({ navigation }) {
   const [hasPermission, setHasPermission] = React.useState(false);
   const devices = useCameraDevices();
   const device = devices.back;
+  const cameraRef = React.useRef(null);
 
   const [frameProcessor, barcodes] = useScanBarcodes([BarcodeFormat.QR_CODE], {
     checkInverted: true,
   });
-
-  // Alternatively you can use the underlying function:
-  //
-  // const frameProcessor = useFrameProcessor((frame) => {
-  //   'worklet';
-  //   const detectedBarcodes = scanBarcodes(frame, [BarcodeFormat.QR_CODE], { checkInverted: true });
-  //   runOnJS(setBarcodes)(detectedBarcodes);
-  // }, []);
 
   React.useEffect(() => {
     (async () => {
@@ -29,11 +21,21 @@ export default function ScanQRScreen({navigation}) {
     })();
   }, []);
 
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const options = { quality: 0.8, base64: true };
+      const photo = await cameraRef.current.takePhoto(options);
+      console.log(photo);
+      // Save the photo or perform other operations with the captured image
+    }
+  };
+
   return (
     device != null &&
     hasPermission && (
       <>
         <Camera
+          ref={cameraRef}
           style={StyleSheet.absoluteFill}
           device={device}
           isActive={true}
@@ -45,6 +47,11 @@ export default function ScanQRScreen({navigation}) {
             {barcode.displayValue}
           </Text>
         ))}
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.captureButton} onPress={takePicture}>
+            <Text style={styles.buttonText}>Take Picture</Text>
+          </TouchableOpacity>
+        </View>
       </>
     )
   );
@@ -55,5 +62,20 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: 'white',
     fontWeight: 'bold',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 30,
+    alignSelf: 'center',
+  },
+  captureButton: {
+    backgroundColor: 'black',
+    borderRadius: 5,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
   },
 });
