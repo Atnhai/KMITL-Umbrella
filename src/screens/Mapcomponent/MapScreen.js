7d88d6f6fccafa481a7d414bdf908422bcd5a949
Atnhai <atnhai@gmail.com>
2023-04-17 02:32:48 +0700

    update rent pop up page

--- src/screens/Mapcomponent/MapScreen.js
+++ src/screens/Mapcomponent/MapScreen.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import {
   Text,
   View,
@@ -9,6 +9,7 @@ import {
   TouchableOpacity,
   Alert,
   TextInput,
+  Modal,
 } from 'react-native';
 import Logo from '../../../assets/images/search.png';
 import {useNavigation} from '@react-navigation/native';
@@ -18,19 +19,34 @@ import {FullWindowOverlay} from 'react-native-screens';
 
 enableLatestRenderer();
 
-export default function MapScreen({navigation}) {
+export default function MapScreen({ navigation }) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const showModal = (item) => {
+    setSelectedItem(item);
+    setModalVisible(true);
+  };
+
   const item = [
     {
       id: 1,
       latitude: 13.729249840361328,
       longitude: 100.77563323749371,
+      place: 'ECC Building',
+      availableUmbrellas: 5,
+      image: require('../../../assets/images/ecc.jpg'),
     },
     {
       id: 2,
       latitude: 13.726573105186487,
       longitude: 100.77497633816488,
+      place: 'HM Building',
+      availableUmbrellas: 3,
+      image: require('../../../assets/images/hm.jpg'),
     },
   ];
+
   return (
     <View style={styles.container}>
       <View style={styles.views}>
@@ -55,10 +71,35 @@ export default function MapScreen({navigation}) {
                 latitude: item.latitude,
                 longitude: item.longitude,
               }}
+              onPress={() => showModal(item)}
             />
           ))}
         </MapView>
       </View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.modalView}>
+          <Text style={styles.modalText}>{selectedItem?.place}</Text>
+          <Image
+             style={styles.modalImage}
+             source={selectedItem ? selectedItem.image : null}
+          />
+          <Text style={styles.modalText}>
+            Umbrellas available: {selectedItem?.availableUmbrellas}
+          </Text>
+          <Button title="Rent" onPress={() => Alert.alert('Rent clicked')} />
+          <TouchableOpacity
+            style={{ ...styles.modalCloseButton }}
+            onPress={() => setModalVisible(!modalVisible)}>
+            <Text style={styles.modalCloseButtonText}>Back</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -107,5 +148,43 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 40,
     //justifycontent: 'flex-end',
+  },
+  modalView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#FAC983',
+    width: '90%',
+    alignSelf: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+    fontSize: 18,
+  },
+  modalImage: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'cover',
+    marginBottom: 15,
+  },
+  modalCloseButton: {
+    backgroundColor: '#FAC983',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    position: 'absolute',
+    top: 10,
+    left: 10,
+  },
+  modalCloseButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
